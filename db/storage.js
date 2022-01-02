123315c7a1ac85cdcb4b05128f221891bed00f64
bradleytevans <bradleyt.evans@gmail.com>
2022-01-02 13:41:59 -0500

    added the functions needed to give each new note a unique id

--- db/storage.js
+++ db/storage.js
@@ -1,3 +1,41 @@
 const util = require('util');
 const fs = require('fs');
-const uuid = require('uuid/v1');
+const uuidv4 = require('uuid/v4');
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+    read() {
+        return readFileAsync('db/db.json', 'utf8');
+    }
+
+    write(note) {
+        return writeFileAsync('db/db.json', JSON.stringify(note));
+    }
+
+    getNotes() {
+        return this.read().then((notes) => {
+            let parsedNotes;
+            try {
+                parsedNotes = [].concat(JSON.parse(notes));
+            } catch (err) {
+                parsedNotes = [];
+            }
+            return parsedNotes;
+        });
+    }
+    
+    addNotes(note) {
+        const { title, text } = note;
+        if (!title || !text) {
+            throw new Error("Note 'title' and 'text' cannot be blank");
+        }
+        const newNote = { title, text, id: uuidv4() };
+        return this.getNotes()
+            .then((notes) => [...notes, newNote])
+            .then((updatedNotes) => this.write(updatedNotes))
+            .then(() => newNote);
+    }
+}
+
+module.exports = new Store();
