4f06465feb3e5c5bfac18bb3dba16b1a39fb888f
bradleytevans <bradleyt.evans@gmail.com>
2022-01-02 13:11:14 -0500

    added routes for the api for getting notes from and adding notes to the database

---
+++ db/storage.js
@@ -0,0 +1,3 @@
+const util = require('util');
+const fs = require('fs');
+const uuid = require('uuid/v1');
---
+++ routes/apiRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const store = require('../db/storage');
+
+router.get('/notes', (req, res) =>{
+    store
+        .getNotes()
+        .then((notes) => {
+            return res.json(notes);
+            })
+            .catch((err) => res.status(500).json(err));
+}) ;
+
+router.post('/notes', (req, res) => {
+    store
+    .addNotes(req.body)
+    .then((notes) => res.json(notes))
+    .catch((err) => res.status(500).json(err));
+});
+
+module. exports = router;
